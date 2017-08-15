477edb2b1e3a2ecc738abd7383f1e641b78688c8
BigfootN <bigfoot_arch@protonmail.com>
2017-08-15 19:28:03 +0200

    update pong.*

---
+++ src/pong.h
@@ -0,0 +1,15 @@
+#ifndef PONG_H
+#define PONG_H
+
+#include "color.h"
+#include "pong_ball.h"
+#include "pong_client.h"
+#include "pong_event.h"
+#include "pong_field.h"
+#include "pong_init.h"
+#include "pong_opt.h"
+#include "pong_paddle.h"
+#include "pong_server.h"
+#include "pong_window.h"
+
+#endif
